ec616846db232ffd5fd19b79a619477350a56f86
Red <red.sarouf2014@gmail.com>
2023-08-05 06:18:06 -0400

    add python io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    text_file = open(f"{file_name}.txt", mode='w', encoding='utf-8')
+    text_file.write(file_content)
+    return text_file
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as text_file:
+        text_file.write(f"{append_content}")
+    return text_file
 
-def read_file(file_name):
-    pass
+def read_file(file_name="lib/test_file"):
+    with open(f"{file_name}.txt", encoding='utf-8') as text_file:
+        return text_file.read()
+    
